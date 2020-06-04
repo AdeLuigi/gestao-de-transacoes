7b6f531a9c77b654b3eb2607d7828bc6d7738f6c
adeluigi <ademario63@gmail.com>
2020-06-04 15:42:34 -0300

    implementação da funcionalidade de deletar transações

--- src/services/DeleteTransactionService.ts
+++ src/services/DeleteTransactionService.ts
@@ -1,8 +1,18 @@
-// import AppError from '../errors/AppError';
+import { getRepository, DeleteResult } from 'typeorm';
+import AppError from '../errors/AppError';
+
+import Transaction from '../models/Transaction';
 
 class DeleteTransactionService {
-  public async execute(): Promise<void> {
-    // TODO
+  public async execute(id: string): Promise<DeleteResult> {
+    const transactionRepository = getRepository(Transaction);
+
+    const transactionDeleted = await transactionRepository.delete(id);
+
+    if (transactionDeleted.affected === 0) {
+      throw new AppError('this id not is valid for delete');
+    }
+    return transactionDeleted;
   }
 }
 
